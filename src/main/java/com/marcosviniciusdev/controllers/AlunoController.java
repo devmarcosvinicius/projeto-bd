3df9f86a32a080ef7d5efaa813dc458280a87fbb
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-18 13:09:27 -0300

    Changes made in AlunoController

--- src/main/java/com/marcosviniciusdev/controllers/AlunoController.java
+++ src/main/java/com/marcosviniciusdev/controllers/AlunoController.java
@@ -1,15 +1,30 @@
 package com.marcosviniciusdev.controllers;
 
+import com.marcosviniciusdev.models.Aluno;
 import com.marcosviniciusdev.repositories.AlunoRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
-    AlunoRepository alunoRepository;
+    private AlunoRepository alunoRepository;
 
-    
+    @GetMapping
+    public List<Aluno> getAlunos() {
+        return this.alunoRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Aluno> create(@RequestBody Aluno aluno) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.alunoRepository.save(aluno));
+    }
 
 }
